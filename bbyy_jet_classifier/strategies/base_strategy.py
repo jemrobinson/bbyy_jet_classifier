a3730cc9b5ece4b7fdc60a0912c3b8f200e6276c
James Robinson <james.em.robinson@gmail.com>
2016-04-26 14:47:59 +0200

    Added ability to load previous output and run in classification-only mode by setting ftrain=0

--- bbyy_jet_classifier/strategies/base_strategy.py
+++ bbyy_jet_classifier/strategies/base_strategy.py
@@ -8,6 +8,7 @@ class BaseStrategy(object) :
     self.output_directory = output_directory if output_directory is not None else self.default_output_location
     self.ensure_directory( self.output_directory )
 
+
   def ensure_directory( self, directory ) :
     if not os.path.exists(directory):
       os.makedirs(directory)
@@ -17,11 +18,16 @@ class BaseStrategy(object) :
     self.variable_dict = root2python.get_branch_info( input_filename, correct_treename, excluded_variables )
     self.correct_array = root2rec( input_filename, correct_treename, branches=self.variable_dict.keys() )
     self.incorrect_array = root2rec( input_filename, incorrect_treename, branches=self.variable_dict.keys() )
-    self.correct_no_weights = self.correct_array[ [name for name in self.variable_dict.keys() if name != "event_weight"] ]
-    self.incorrect_no_weights = self.incorrect_array[ [name for name in self.variable_dict.keys() if name != "event_weight"] ]
+    self.classification_variables = sorted( [ name for name in self.variable_dict.keys() if name != "event_weight" ] )
+    self.correct_no_weights = self.correct_array[self.classification_variables]
+    self.incorrect_no_weights = self.incorrect_array[self.classification_variables]
     self.correct_weights_only = self.correct_array[ ["event_weight"] ]
     self.incorrect_weights_only = self.incorrect_array[ ["event_weight"] ]
 
 
-  def run( self, training_fraction ) :
+  def train_and_test( self, training_fraction ) :
+    raise NotImplementedError( "Must be implemented by child class!" )
+
+
+  def test_only( self ) :
     raise NotImplementedError( "Must be implemented by child class!" )
--- run_classifier.py
+++ run_classifier.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
 import argparse
 from bbyy_jet_classifier import strategies, plotting
+import logging
 import os
 
 if __name__ == "__main__" :
+  logger = logging.getLogger("RunClassifier")
 
   # -- Parse arguments
   parser = argparse.ArgumentParser( description="Run ML algorithms over ROOT TTree input" )
@@ -22,11 +24,19 @@ if __name__ == "__main__" :
   # -- Construct dictionary of available strategies
   if not args.strategy in strategies.__dict__.keys() : raise AttributeError( "{} is not a valid strategy".format( args.strategy ) )
 
-  # -- Run appropriate strategy
+  # -- Load data for appropriate strategy
   ML_strategy = getattr(strategies,args.strategy)( args.output )
   ML_strategy.load_data( args.input, args.correct_tree, args.incorrect_tree, args.exclude )
-  ML_strategy.run( args.ftrain )
 
-  # -- Plot distributions
-  plotting.plot_inputs( ML_strategy )
-  plotting.plot_outputs( ML_strategy )
+  # -- Run appropriate strategy
+  if args.ftrain > 0 :
+    logger.info( "Preparing to train with {}% of events and then test with the remainder".format( int(100*args.ftrain) ) )
+    ML_strategy.train_and_test( args.ftrain )
+    # -- Plot distributions
+    plotting.plot_training_inputs( ML_strategy )
+    plotting.plot_training_outputs( ML_strategy )
+  else :
+    logger.info( "Preparing to use 100% of sample as testing input" )
+    ML_strategy.test_only()
+    # -- Plot distributions
+    plotting.plot_testing_outputs( ML_strategy, args.input.replace(".root","") )
