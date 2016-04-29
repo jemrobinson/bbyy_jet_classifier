d9150401c162ca23e736a7841379006dcfb582f9
Michela Paganini <micky.91@hotmail.com>
2016-04-29 16:54:21 +0200

    Add old strategies (mHmatch, pThigh)

--- bbyy_jet_classifier/strategies/root_tmva.py
+++ bbyy_jet_classifier/strategies/root_tmva.py
@@ -9,18 +9,29 @@ from root_numpy import array2tree, root2rec, tree2array
 from root_numpy.tmva import add_classification_events, evaluate_reader
 from numpy.lib import recfunctions
 
-class RootTMVA(BaseStrategy) :
+class RootTMVA(BaseStrategy):
   default_output_location = "output/RootTMVA"
 
-
-  def train( self, X_train, y_train, w_train ):
-    f_output = ROOT.TFile( "{}/TMVA_output.root".format( self.output_directory ), "RECREATE" )
-    factory = ROOT.TMVA.Factory( "TMVAClassification", f_output, "AnalysisType=Classification" )
+  def train(self, X_train, y_train, w_train):
+    '''
+    Definition:
+    -----------
+        Training method for RootTMVA; it saves the model into the 'weights' sub-folder
+    Args:
+    -----
+        X_train = the features matrix with events for training, of dimensions (n_events, n_features) 
+        y_train = the target array with events for training, of dimensions (n_events)
+        w_train = the array of weights for training events, of dimensions (n_events)
+    '''
+    f_output = ROOT.TFile("{}/TMVA_output.root".format(self.output_directory), "RECREATE")
+    factory = ROOT.TMVA.Factory("TMVAClassification", f_output, "AnalysisType=Classification")
 
     # -- Add variables to the factory:
-    for v_name, v_type in self.variable_dict.items() :
-        if v_name != "event_weight" : factory.AddVariable( v_name, v_type )
-    
+    # for v_name, v_type in self.variable_dict.items() :
+    #     if v_name != "event_weight": factory.AddVariable( v_name, v_type)
+    for v_name in self.classification_variables:
+        factory.AddVariable(v_name, self.variable_dict[v_name])
+
     # Call root_numpy's utility functions to add events from the arrays
     add_classification_events(factory, X_train, y_train, weights=w_train)
     add_classification_events(factory, X_train[0:20], y_train[0:20], weights=w_train[0:20], test=True) # need to put in something or TMVA will complain
@@ -29,22 +40,34 @@ class RootTMVA(BaseStrategy) :
     factory.PrepareTrainingAndTestTree(ROOT.TCut('1'), 'NormMode=EqualNumEvents')
 
     #-- Define methods:
-    BDT_method = factory.BookMethod( ROOT.TMVA.Types.kBDT, "BDT", ":".join(
-      [ "NTrees=800", "MinNodeSize=5", "MaxDepth=10", "BoostType=Grad", "SeparationType=GiniIndex" ]
-    ))
+    BDT_method = factory.BookMethod(ROOT.TMVA.Types.kBDT, "BDT", ":".join(
+        ["NTrees=800", "MinNodeSize=5", "MaxDepth=10", "BoostType=Grad", "SeparationType=GiniIndex"]
+        ))
 
     # -- Where stuff actually happens:
     factory.TrainAllMethods()
 
       # -- Organize output:
-    if os.path.isdir( "{}/weights".format(self.output_directory) ) :
-        shutil.rmtree( "{}/weights".format(self.output_directory) )
-    shutil.move( "weights", self.output_directory )
-
-
+    if os.path.isdir("{}/weights".format(self.output_directory)):
+        shutil.rmtree("{}/weights".format(self.output_directory))
+    shutil.move("weights", self.output_directory)
   
-  def test( self, X, y, w, process) :
 
+  def test(self, X, y, w, process):
+    '''
+    Definition:
+    -----------
+        Testing method for RootTMVA; it loads the latest model from the 'weights' sub-folder
+    Args:
+    -----
+        X = the features matrix with events to test performance on, of dimensions (n_events, n_features) 
+        y = the target array with events to test performance on, of dimensions (n_events)
+        w = the array of weights of the events to test performance on, of dimensions (n_events)
+        process = string to identify whether we are evaluating performance on the train or test set, usually 'training' or 'testing'
+    Returns:
+    --------
+        yhat = the array of BDT outputs, of dimensions (n_events)
+    '''
     logging.getLogger("TMVA_BDT").info("Evaluating Performance...")
 
     # -- Setup dictionary of variable names to ROOT-accessible arrays
@@ -52,22 +75,16 @@ class RootTMVA(BaseStrategy) :
 
     # -- Construct reader and add variables to it:
     reader = ROOT.TMVA.Reader()
-    for v_name in self.classification_variables :
+    for v_name in self.classification_variables:
       #variables[v_name] = array.array( root2python.CHAR_2_ARRAYTYPE[self.variable_dict[v_name]] ,[0])
       #reader.AddVariable( v_name, variables[v_name] )
       #^^ this gives error: --- <FATAL> Reader                   : Reader::AddVariable( const TString& expression, Int_t* datalink ), this function is deprecated, please provide all variables to the reader as floats
-      reader.AddVariable(v_name, array.array('f', [0] ) )
+      reader.AddVariable(v_name, array.array('f', [0]))
 
     # -- Load TMVA results
     reader.BookMVA("BDT", "{}/weights/TMVAClassification_BDT.weights.xml".format(self.output_directory))
 
-    yhat = evaluate_reader(reader, 'BDT', X)
-
-    # # -- Log classification scores
-    # logging.getLogger("TMVA_BDT").info( "{} accuracy = {:.2f}%".format(process, 100 * classifier.score( X, y, sample_weight=w)) )
-    # logging.getLogger("TMVA_BDT").info( classification_report( y, classifier.predict(X), target_names=["correct","incorrect"], sample_weight=w) )
-    
-
+    yhat = evaluate_reader(reader, 'BDT', X)    
     return yhat
 
    
--- bbyy_jet_classifier/strategies/skl_BDT.py
+++ bbyy_jet_classifier/strategies/skl_BDT.py
@@ -2,8 +2,7 @@ from . import BaseStrategy
 from ..adaptors import root2python
 import logging
 import numpy as np
-#from root_numpy import rec2array
-#from sklearn.cross_validation import train_test_split
+import cPickle
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.externals import joblib
 from sklearn.metrics import classification_report
@@ -11,29 +10,52 @@ from sklearn.metrics import classification_report
 class sklBDT(BaseStrategy) :
   default_output_location = "output/sklBDT"
 
-  def train( self, X_train, y_train, w_train ):
-
+  def train(self, X_train, y_train, w_train):
+    '''
+    Definition:
+    -----------
+        Training method for sklBDT; it pickles the model into the 'pickle' sub-folder
+    Args:
+    -----
+        X_train = the features matrix with events for training, of dimensions (n_events, n_features) 
+        y_train = the target array with events for training, of dimensions (n_events)
+        w_train = the array of weights for training events, of dimensions (n_events)
+    '''
     # -- Train:
-    logging.getLogger("sklBDT").info( "Training..." )
+    logging.getLogger("sklBDT").info("Training...")
     classifier = GradientBoostingClassifier(n_estimators=200, min_samples_split=2, max_depth=10, verbose=1)
-    classifier.fit( X_train, y_train, sample_weight=w_train )
+    classifier.fit(X_train, y_train, sample_weight=w_train)
 
     # -- Dump output to pickle
-    self.ensure_directory( "{}/pickle/".format(self.output_directory) )
-    joblib.dump( classifier, "{}/pickle/sklBDT_output.pkl".format(self.output_directory) )
+    self.ensure_directory("{}/pickle/".format(self.output_directory))
+    joblib.dump(classifier, "{}/pickle/sklBDT_clf.pkl".format(self.output_directory), protocol=cPickle.HIGHEST_PROTOCOL)
 
 
-  def test( self, X, y, w, process) :
-    logging.getLogger("sklBDT").info( "Evaluating Performance..." )
+  def test(self, X, y, w, process):
+    '''
+    Definition:
+    -----------
+        Testing method for sklBDT; it loads the latest model from the 'pickle' sub-folder
+    Args:
+    -----
+        X = the features matrix with events to test performance on, of dimensions (n_events, n_features) 
+        y = the target array with events to test performance on, of dimensions (n_events)
+        w = the array of weights of the events to test performance on, of dimensions (n_events)
+        process = string to identify whether we are evaluating performance on the train or test set, usually 'training' or 'testing'
+    Returns:
+    --------
+        yhat = the array of BDT outputs corresponding to the P(signal), of dimensions (n_events)
+    '''
+    logging.getLogger("sklBDT").info("Evaluating Performance...")
 
     # -- Load scikit classifier
-    classifier = joblib.load( "{}/pickle/sklBDT_output.pkl".format(self.output_directory) )
+    classifier = joblib.load("{}/pickle/sklBDT_clf.pkl".format(self.output_directory))
 
     # -- Get classifier predictions
     yhat = classifier.predict_proba(X)[:, 1]
 
     # -- Log classification scores
-    logging.getLogger("sklBDT").info( "{} accuracy = {:.2f}%".format(process, 100 * classifier.score( X, y, sample_weight=w)) )
-    logging.getLogger("sklBDT").info( classification_report( y, classifier.predict(X), target_names=["correct","incorrect"], sample_weight=w) )
+    logging.getLogger("sklBDT").info("{} accuracy = {:.2f}%".format(process, 100 * classifier.score(X, y, sample_weight=w)) )
+    logging.getLogger("sklBDT").info(classification_report(y, classifier.predict(X), target_names=["correct", "incorrect"], sample_weight=w))
     
     return yhat
